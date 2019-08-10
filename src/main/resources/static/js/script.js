4b146b3fd588a584df8274a4452719961b103d4a
Chrisphine10 <ondiekchrisphine@gmail.com>
2019-08-10 12:26:35 +0300

    error

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,22 +1,5 @@
 'use strict';
-const express = require('express');
-const app = express();
-const router = express.Router();
 
-app.use(express.static('public'));
-router.get('*',function(req, res) {
-  res.sendFile(path.join(__dirname, './public/index'));
-});
-
-included process.env PORT variable for herokus specific port needed to deploy
-    const PORT = process.env.PORT || 8000;
-    app.listen(PORT, function(err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('listening on port 8000');
-    });
 var welcomeForm = document.querySelector('#welcomeForm');
 var dialogueForm = document.querySelector('#dialogueForm');
 
